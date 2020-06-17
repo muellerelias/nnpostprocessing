6074eda531a49453e1abd437e7a418a3a7cc0221
elias <elias@mumail.de>
2020-06-17 10:57:19 +0200

    bearbeitung von datareader

--- dataset/read_csv_files.py
+++ dataset/read_csv_files.py
@@ -1,5 +1,7 @@
 import csv
-import os 
+import os
+from itertools import groupby
+from datetime import datetime, date, timedelta  
 
 path='/home/elias/Nextcloud/1.Masterarbeit/Daten/2020_MA_Elias'
 
@@ -7,11 +9,19 @@ def read_csv(name):
     list = []
     with open(os.path.join(path, name ), newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        rownr = 0
         for row in spamreader:
-            list.append(row)
+            if rownr is not 0:
+                for i in range(len(row)):
+                    if i==0:
+                        row[i]=int(row[i])
+                    if i not in [0,1,2]:
+                        row[i]=float(row[i])
+                list.append(row)
+            rownr+=1
     return list[1:]
 
-def read_csv_with_label(name):
+def read_csv_with_title(name):
     list = []
     with open(os.path.join(path, name ), newline='') as csvfile:
         spamreader = csv.DictReader(csvfile, delimiter=',', quotechar='|')
@@ -56,8 +66,37 @@ def add_label_dictionary(listitem):
     list.append({"GL":listitem[32]})
     return list
 
-def filterCountry(data, country):
+def filter_country(data, country):
     return list(filter(lambda x: True if x[2]==country else False, data))
+
+def get_labes_by_country(data, id, days):
+    countries=[]
+    for key, group in groupby(sorted(data, key=lambda x: x[2]), lambda x: x[2]):
+        countries.append(list(group))
+    resultlist = []
+    for country in countries:
+        result_country = get_labes(country, id, days)
+        resultlist.append(result_country)
+    return resultlist
+
+def get_labes(data, id, days):
+    start = datetime.strptime(data[id][1], '%Y-%m-%d').date()
+    dates = []
+    date = start
+    for i in range(days):
+        dates.append(date.strftime("%Y-%m-%d"))
+        date = date +timedelta(days=1)
+    filterddata = list(filter(lambda x: True if x[1] in dates else False, data))
+    T2M = 0 
+    U10M = 0
+    V10M = 0 
+    RTOT = 0
+    for i in  filterddata:
+        T2M  += i[3]
+        U10M += i[4]
+        V10M += i[5]
+        RTOT += i[6]
+    return [i[2], start.strftime("%Y-%m-%d"), date.strftime("%Y-%m-%d"),  T2M/len(filterddata), U10M/len(filterddata), V10M/len(filterddata), RTOT]
 
 def main():
     read_csv('ecmwf_PF_03_240.csv')
--- train.py
+++ train.py
@@ -1,13 +1,13 @@
 import numpy as np
 import os
 import time
-
 import tensorflow as tf
 import dataset.read_csv_files as provider
 
 data = provider.read_csv('ecmwf_PF_03_240.csv')
 
+print(data[1])
 
-data1 = provider.filterCountry(data, 'Germany')
-
-print(data1[505])
+data1 = provider.filter_country(data, 'Germany')
+print(provider.get_labes(data1, 0, 363))
+print(provider.get_labes_by_country(data1, 0, 363))
