af8c363a4cff63fc935aa74afcb263c7611eb010
elias <elias@mumail.de>
2020-12-14 12:32:21 +0100

    build the numpy arrays out of the csv files

--- build_np_array.sh
+++ build_np_array.sh
@@ -4,3 +4,4 @@
 python3 dataset/build_np_array.py \
     --dataset_dir /home/elias/Nextcloud/1.Masterarbeit/Daten/15days/2020_MA_Elias/ \
     --np_dir /home/elias/Nextcloud/1.Masterarbeit/Daten/15days/vorverarbeitetRegime/
+    --horzion 10days
--- dataset/build_np_array.py
+++ dataset/build_np_array.py
@@ -22,16 +22,20 @@ parser.add_argument("--np_dir", dest="np_dir", metavar="FILE",
                     help="folder where to store the numpy arrays")
 
 
-"""
-parser.add_argument("--normalization", dest="normalization", default=False,  action='store_true',
-                    help="Activate normalization of the data")
-""" 
+parser.add_argument("--horzion", dest="horzion", required=True,
+                    help="forecast horizon: 10days or 15days", type=str)
 
 args = parser.parse_args()
 
 
 def main(args):
-    fileglob = glob.glob(os.path.join(args.dataset_dir, 'ecmwf_*_360.csv'))
+    if args.horizon =='10days':
+        fileglob = glob.glob(os.path.join(args.dataset_dir, 'ecmwf_*_240.csv'))
+    elif args.horizon =='15days':
+        fileglob = glob.glob(os.path.join(args.dataset_dir, 'ecmwf_*_360.csv'))
+    else:
+        raise Exception("Wrong forecast horizon")
+
     start = datetime.datetime.now()
     train_set = []
     train_set_all = []
@@ -41,35 +45,39 @@ def main(args):
     # read all data and concat the train set for mean and std
     for path in fileglob:
         df = pd.read_csv(path,  index_col=0)
+        # train set
         train = df[(df['init date'] > '1997-01-01') 
                             & (df['init date'] < '2009-12-31')]
+        train_set_all.append(train) # for mean and std
+        train_set.append(train.to_numpy())
+        # validation set
         valid_set.append(df[(df['init date'] > '2010-01-01')
                             & (df['init date'] < '2013-12-31')].to_numpy())
+        #test set
         test_set.append(df[(df['init date'] > '2014-01-01')
                            & (df['init date'] < '2017-12-31')].to_numpy())
 
-        train_set_all.append(train)
-        train_set.append(train.to_numpy())
-    
     print('[INFO] starting calculating mean and std...')
 
     # calculate mean and std
     train_set_all = pd.concat(train_set_all, axis=0).to_numpy()
 
+    # delete country and date for mean and std calculation
     train_set_numpy = []
     for item in train_set_all:
         train_set_numpy.append(item[2:])
 
     train_set_all = np.array(train_set_numpy, dtype='float64')
+    # add to first elements because of same index like the other data
     train_mean = np.concatenate(([0, 0], train_set_all.mean(axis=0)), axis=0)
-    train_std = np.concatenate(([0, 0], train_set_all.std(axis=0)),  axis=0)
+    train_std  = np.concatenate(([0, 0], train_set_all.std(axis=0)) , axis=0)
 
     # save the mean and std
     np.save(os.path.join(args.np_dir, 'train_mean.npy'), train_mean)
     np.save(os.path.join(args.np_dir, 'train_std.npy'), train_std)
 
-    print('[INFO] starting normalizing and transforming to model tensor...')
-
+    print('[INFO] starting transforming to model tensor...')
+    # calculate each set
     train_set = convert_to_model_data(
         train_set, train_mean, train_std)
     valid_set = convert_to_model_data(
@@ -77,7 +85,7 @@ def main(args):
     test_set = convert_to_model_data(
         test_set, train_mean, train_std)
 
-    print('[INFO] starting saving dataset split..')
+    # saving the sets
     np.save(os.path.join(args.np_dir, 'train_set.npy'), train_set)
     np.save(os.path.join(args.np_dir, 'valid_set.npy'), valid_set)
     np.save(os.path.join(args.np_dir, 'test_set.npy'), test_set)
@@ -87,33 +95,44 @@ def main(args):
 
 def convert_to_model_data(set, mean, std):
     data = []
+    # iterate through all records
     for i in range(len(set[0])):
         row = []
-        # first element [date_transformed, country_id, AT, ZO, ZOEA, AR, ZOWE, BL, GL]
+        # transforme the date
         date = convert_date(set[0][i][0])
+        # transform the country string in ID
         country = convert_country(set[0][i][1])
+        
         matrix_data = []
         regime_data = []
-        
+        # iterate through all 11 ensemble members
         for file in set:
+            # NWP with normalisation
             matrix_data.append((file[i][6:25]-mean[6:25])/std[6:25])
+            # Regimes without normalisation
             regime_data.append(file[i][25:32])  
 
+        #All labels
         label = np.array(set[0][i][2:6], dtype='float64')
 
+        # transform regime ensemble members to mean and std
         regime_data = np.array(regime_data, dtype='float64')
         vector = np.concatenate((np.array([country, date]), regime_data.mean(axis=0), regime_data.std(axis=0)), axis=0)
-        matrix_data = np.array(matrix_data, dtype='float64')
 
+        # transform NWPs ensemble members to mean and std
+        matrix_data = np.array(matrix_data, dtype='float64')
         matrix = np.array(
             [matrix_data.mean(axis=0), matrix_data.std(axis=0)], dtype='float64')
-        
+
+        # 2m temperatur ensample sample        
         ensemble = []
         for file in set:
             ensemble.append(file[i][22])
 
+        # calculate raw ensemble cprs
         Crps = crps.ensemble(set[0][i][2], ensemble)
 
+        # collecting one record
         row.append(vector)
         row.append(matrix)
         row.append(label)
